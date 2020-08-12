d743ca14ff00dadc7838b70853cf099de5a4d70f
zhou <1079508810@qq.com>
2020-08-12 11:51:29 +0800

    功能完成

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,111 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <span>请输入ShowDoc接口文档:</span>
+    <br>
+    <br>
+    <textarea ref="et_sd" cols="160" rows="12" v-model="sd_text" placeholder=""></textarea>
+    <br>
+    <br>
+
+    <button ref="bt_sd_to_retrofit" v-on:click="sd_to_retrofit()">代码转化</button>
+    <br>
+    <br>
+
+    <span>转成Retrofit接口代码:</span>
+    <br>
+    <br>
+    <textarea ref="et_retrofit" cols="160" rows="12"  v-model="retrofit_text" placeholder=""></textarea>
+    <br>
+    <br>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return{
+      message:'123',
+      sd_text:'',
+      retrofit_text:''
+    }
+  },
+
+  methods:{
+    sd_to_retrofit(){
+      let sd_text = '';
+      sd_text = this.sd_text;
+
+      let snsArr;
+      snsArr=sd_text.split(/[(\r\n)\r\n]+/);
+
+      for (let i = 0; i< snsArr.length; i++){
+        console.log(snsArr[i]);
+      }
+
+      if (snsArr.length == 11){
+        let a = snsArr[8].trim().split(/\s+/);
+        console.log(a);
+        let b = snsArr[9].trim().split(/\s+/);
+        console.log(b);
+        let c = snsArr[10].trim().split(/\s+/);
+        console.log(c);
+
+        this.retrofit_text = "    /** \n " +
+                "     * "+snsArr[1]+" \n " +
+                "     * "+snsArr[8]+" \n " +
+                "     * "+snsArr[9]+" \n " +
+                "     * "+snsArr[10]+" \n " +
+                "     */ \n " +
+                "    @FormUrlEncoded \n "+
+                "    @"+"POST"+"(\""+snsArr[3]+"\") \n " +
+                "    Observable<BaseResponse<Bean>> "+snsArr[3].replace("/","")+"(@Field(\""+a[0]+"\") "+(a[2].includes('s')?"String":a[2])+" "+a[0]+", \n" +
+                "                                                          @Field(\""+b[0]+"\") "+(b[2].includes('s')?"String":b[2])+" "+b[0]+", \n" +
+                "                                                          @Field(\""+c[0]+"\") "+(c[2].includes('s')?"String":c[2])+" "+c[0]+");";
+      }else if (snsArr.length == 9){
+        let a = snsArr[8].trim().split(/\s+/);
+        console.log(a);
+
+        this.retrofit_text = "    /** \n " +
+                "     * "+snsArr[1]+" \n " +
+                "     * "+snsArr[8]+" \n " +
+                "     */ \n " +
+                "    @FormUrlEncoded \n "+
+                "    @"+"POST"+"(\""+snsArr[3]+"\") \n " +
+                "    Observable<BaseResponse<Bean>> "+snsArr[3].replace("/","")+"(@Field(\""+a[0]+"\") "+(a[2].includes('s')?"String":a[2])+" "+a[0]+");";
+
+      }else if (snsArr.length == 10){
+        let a = snsArr[8].trim().split(/\s+/);
+        console.log(a);
+        let b = snsArr[9].trim().split(/\s+/);
+        console.log(b);
+
+        this.retrofit_text = "    /** \n " +
+                "     * "+snsArr[1]+" \n " +
+                "     * "+snsArr[8]+" \n " +
+                "     * "+snsArr[9]+" \n " +
+                "     */ \n " +
+                "    @FormUrlEncoded \n "+
+                "    @"+"POST"+"(\""+snsArr[3]+"\") \n " +
+                "    Observable<BaseResponse<Bean>> "+snsArr[3].replace("/","")+"(@Field(\""+a[0]+"\") "+(a[2].includes('s')?"String":a[2])+" "+a[0]+", \n" +
+                "                                                          @Field(\""+b[0]+"\") "+(b[2].includes('s')?"String":b[2])+" "+b[0]+");";
+
+      }else {
+
+        this.retrofit_text = "    /** \n " +
+                "     * "+snsArr[1]+" \n " +
+                "     */ \n " +
+                "    @FormUrlEncoded \n "+
+                "    @"+"POST"+"(\""+snsArr[3]+"\") \n " +
+                "    Observable<BaseResponse<Bean>> "+snsArr[3].replace("/","")+"();";
+
+      }
+
+
+      console.log(sd_text);
+    }
+
   }
 }
 </script>
